01b4bc9759d396cd05b905abb5f8578998949b7b
Bin <iamsk.info@gmail.com>
2018-09-23 13:35:43 +0800

    support other databases, not just pg

--- example/project/settings.py
+++ example/project/settings.py
@@ -67,8 +67,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_admin_json_editor',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
